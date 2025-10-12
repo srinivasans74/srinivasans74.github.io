b5fa11b313284c35be4d7016c2f44c42a70becd6
srinivasans74 <47769589+srinivasans74@users.noreply.github.com>
2025-10-12 18:47:01 +0000

    Deploying to gh-pages from @ srinivasans74/srinivasans74.github.io@72efcd56932fa650feeca2ee078490a528b2fb45 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "srinivasans74/srinivasans74.github.io-",
+    "data-repo": "srinivasans74/srinivasans74.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
