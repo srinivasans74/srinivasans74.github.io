374c03edfdc5d50b25027aa2776809019ed9ab64
srinivasans74 <47769589+srinivasans74@users.noreply.github.com>
2025-12-03 22:51:25 +0000

    Deploying to gh-pages from @ srinivasans74/srinivasans74.github.io@f9229fe9912a79787f5d80dd8d8475d530d8a7b1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,6 +44,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-teaching",
+          title: "teaching",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/Achievements/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
