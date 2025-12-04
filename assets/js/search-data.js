698d1477a55759d3c31d80a7b3ec262e02eb95bd
srinivasans74 <47769589+srinivasans74@users.noreply.github.com>
2025-12-04 01:50:52 +0000

    Deploying to gh-pages from @ srinivasans74/srinivasans74.github.io@dde9a25133be985de44410f5369ad4fe3ae13c91 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,8 +44,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
+        },{id: "nav-achievements",
+          title: "Achievements",
           description: "",
           section: "Navigation",
           handler: () => {
